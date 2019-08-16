2a4bcf3397ef5cf4da4ff71e213361df51bf1459
jiali0605 <1078867133@qq.com>
2019-08-16 12:14:44 +0800

    1111

---
+++ src/com/wang/new2/News2.java
@@ -0,0 +1,7 @@
+package com.wang.new2;
+
+public class News2 {
+public static void main(String[] args) {
+	System.out.println("第三次测试");
+}
+}
--- src/com/wang/service/Service.java
+++ src/com/wang/service/Service.java
@@ -2,6 +2,6 @@ package com.wang.service;
 
 public class Service {
 public static void main(String[] args) {
-	System.out.println("第三次测试");
+	System.out.println("asfwsdfcas");
 }
 }
